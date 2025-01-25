737f6f181d245c96187fe9924d5a2ff5372ecece
yavan rajput <rajputyavan@gmail.com>
2025-01-25 10:14:36 -0800

    signin update

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,11 +36,11 @@ const loginUser = async (req, res) => {
       }
     );
 
-    const { password: userPassword, ...userData } = user;
+ 
 
     res.status(200).json({
       token,
-      user: userData,
+      user
     });
   } catch (error) {
     res.status(400).json({ error: error.message });
